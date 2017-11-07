62beadd99e64e4e4d264b966673ca33bc498ead8
cazucito <cazucito@FDeux.fdeux>
2017-11-07 15:21:49 -0600

    Hello World in Java stand alone app

---
+++ HelloWorldApp/src/helloworldapp/HelloWorldApp.java
@@ -0,0 +1,17 @@
+package helloworldapp;
+
+/**
+ *
+ * @author 
+ */
+public class HelloWorldApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!!!");
+    }
+    
+}
